0d929efcd8ea381494778c2590aeeaae19f40f53
Jia YuanChao <yuanchao__2106@163.com>
2018-09-03 13:19:20 +0800

    new date

--- BossCommonClass/Classes/NNBRequest/NNBReqestClient/BossMessageRequest/Model/EventExtraModel.h
+++ BossCommonClass/Classes/NNBRequest/NNBReqestClient/BossMessageRequest/Model/EventExtraModel.h
@@ -6,6 +6,8 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "ExamineOrderModel.h"
+#import "ApplicationUrgeRecordModel.h"
 
 @interface EventExtraModel : NSObject
 
@@ -24,6 +26,27 @@
  催办记录ID
  */
 @property (nonatomic, strong) NSString *oa_urge_record_id;
+
+/**
+ 审批单信息
+ */
+@property (nonatomic, strong, readonly) ExamineOrderModel *examineOrderInfo;
+
+/**
+ 审批记录信息
+ */
+@property (nonatomic, strong, readonly) ExamineFlowRecordModel *flowRecordInfo;
+
+/**
+ 催办记录信息
+ */
+@property (nonatomic, strong, readonly) ApplicationUrgeRecordModel *urgeRecordInfo;
+
+/**
+ 数据请求来的需要更新
+ */
+@property (nonatomic, copy) void (^dateNeedUpdateBlock)(EventExtraModel *eventModel);
+
 
 #pragma mark -- boss助理 --- boss 通知 --- end
 
--- BossCommonClass/Classes/NNBRequest/NNBReqestClient/BossMessageRequest/Model/EventExtraModel.m
+++ BossCommonClass/Classes/NNBRequest/NNBReqestClient/BossMessageRequest/Model/EventExtraModel.m
@@ -6,11 +6,89 @@
 //
 
 #import "EventExtraModel.h"
+#import "BossOaExamineRequest.h"
+
+@interface EventExtraModel()
+#pragma mark -- boss助理 --- boss 通知 --- start
+/**
+ 审批单信息
+ */
+@property (nonatomic, strong) ExamineOrderModel *examineOrderInfo;
+
+/**
+ 审批记录信息
+ */
+@property (nonatomic, strong) ExamineFlowRecordModel *flowRecordInfo;
+
+/**
+ 催办记录信息
+ */
+@property (nonatomic, strong) ApplicationUrgeRecordModel *urgeRecordInfo;
+
+@property (nonatomic, assign) BOOL orderInfoResponse;
+
+@property (nonatomic, assign) BOOL urgeRecordInfoResponse;
+
+#pragma mark -- boss助理 --- boss 通知 --- end
+
+@end
 
 @implementation EventExtraModel
 
 - (void)setValue:(id)value forKey:(NSString *)key
 {
+    if ([key isEqualToString:@"oa_application_order_id"]) {
+        if (!value || value == [NSNull null]) {
+            self.orderInfoResponse = YES;
+            [self updateDate];
+            return;
+        }
+        self.oa_application_order_id = value ? [NSString stringWithFormat:@"%@",value] : nil;
+        if (!self.oa_application_order_id) {
+            self.orderInfoResponse = YES;
+            [self updateDate];
+            return;
+        }
+        WS(weakSelf);
+        [BossOaExamineRequest OaExamineRequestGetExamineDetailWithExamineId:self.oa_application_order_id successBlock:^(ExamineOrderModel *examineFlowModel) {
+            weakSelf.orderInfoResponse = YES;
+            if (self.oa_application_record_id) {
+                NSDictionary *recordDic = [JYCSimpleToolClass changeToDictionaryWithArray:examineFlowModel.flow_record_list byKey:@"_id"];
+                self.flowRecordInfo = recordDic[self.oa_application_record_id];
+            }
+            self.examineOrderInfo = examineFlowModel;
+            [weakSelf updateDate];
+        } fail:^(id error) {
+            weakSelf.orderInfoResponse = YES;
+            [weakSelf updateDate];
+        }];
+        return;
+    }
+
+    if ([key isEqualToString:@"oa_urge_record_id"]) {
+        if (!value || value == [NSNull null]) {
+            self.urgeRecordInfoResponse = YES;
+            [self updateDate];
+            return;
+        }
+        self.oa_urge_record_id = value ? [NSString stringWithFormat:@"%@",value] : nil;
+        if (!self.oa_urge_record_id) {
+            self.urgeRecordInfoResponse = YES;
+            [self updateDate];
+            return;
+        }
+        WS(weakSelf);
+        [BossOaExamineRequest OaExamineRequestGetUrgeDetailWithUrgeId:self.oa_urge_record_id orderRecordId:@"" success:^(ApplicationUrgeRecordModel *urgeRecordModel) {
+            weakSelf.urgeRecordInfo = urgeRecordModel;
+            weakSelf.urgeRecordInfoResponse = YES;
+            [weakSelf updateDate];
+        } fail:^(id error) {
+            weakSelf.urgeRecordInfoResponse = YES;
+            [weakSelf updateDate];
+        }];
+        return;
+    }
+
     if (!value || value == [NSNull null]) {
         return;
     }
@@ -23,5 +101,25 @@
     
 }
 
+- (void)updateDate
+{
+    if (self.orderInfoResponse && self.urgeRecordInfoResponse) {
+        if (self.dateNeedUpdateBlock) {
+            self.dateNeedUpdateBlock(self);
+        }
+    }
+}
+
+- (ExamineFlowRecordModel *)flowRecordInfo
+{
+    if (!_flowRecordInfo) {
+        if (_oa_application_record_id && _examineOrderInfo) {
+            NSDictionary *recordDic = [JYCSimpleToolClass changeToDictionaryWithArray:_examineOrderInfo.flow_record_list byKey:@"_id"];
+            _flowRecordInfo = recordDic[self.oa_application_record_id];
+        }
+    }
+    return _flowRecordInfo;
+
+}
 
 @end
