852e984ddc54171fccd1ad78ffb1aaa537d4e046
zx <852153533@qq.com>
2019-05-09 18:02:44 +0800

    修改通讯录

---
+++ BossCommonClass/Classes/Announcement/Model/ContactsChild.h
@@ -0,0 +1,16 @@
+//
+//  ContactsChild.h
+//  AFNetworking
+//
+//  Created by admin on 2019/5/9.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContactsChild : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BossCommonClass/Classes/Announcement/Model/ContactsChild.m
@@ -0,0 +1,12 @@
+//
+//  ContactsChild.m
+//  AFNetworking
+//
+//  Created by admin on 2019/5/9.
+//
+
+#import "ContactsChild.h"
+
+@implementation ContactsChild
+
+@end
