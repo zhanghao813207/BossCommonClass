cd668817b60313d5f3e27775e8afe12c4f45c696
zhanghao813207 <508149687@qq.com>
2019-05-21 10:29:40 +0800

    - 初始委托个户注册模块

---
+++ BossCommonClass/Classes/EntrustAccountRegistration/Controllers/EntrustAccountRegistrationTipVC.h
@@ -0,0 +1,19 @@
+//
+//  EntrustAccountRegistrationTipVC.h
+//  AFNetworking
+//
+//  Created by 张浩 on 2019/5/20.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ 委托个户注册提醒页
+ */
+@interface EntrustAccountRegistrationTipVC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BossCommonClass/Classes/EntrustAccountRegistration/Controllers/EntrustAccountRegistrationTipVC.m
@@ -0,0 +1,36 @@
+//
+//  EntrustAccountRegistrationTipVC.m
+//  AFNetworking
+//
+//  Created by 张浩 on 2019/5/20.
+//
+
+#import "EntrustAccountRegistrationTipVC.h"
+
+@interface EntrustAccountRegistrationTipVC ()
+
+@end
+
+@implementation EntrustAccountRegistrationTipVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (void)initViews{
+    self.title = @"委托个户注册";
+}
+
+@end
