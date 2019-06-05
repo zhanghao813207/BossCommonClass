57e3464ce5e4d7ad3f4b857f5711faa401b6dddb
zhanghao813207 <508149687@qq.com>
2019-06-05 16:14:18 +0800

    - 配置release环境

--- BossCommonClass/Classes/AllDefine/ConstDataDefine/BossConstDataDefine.m
+++ BossCommonClass/Classes/AllDefine/ConstDataDefine/BossConstDataDefine.m
@@ -48,33 +48,33 @@
 #else
 
 #ifdef kBossOwner
-// 生产环境 Boss老板
-NSString *const BossBasicURL = @" https://qlife-api.aoaosong.com";
-NSString *const BossBasicURLV2 = @" https://qlife-api.aoaosong.com";
-NSString *const BossBasicPrefix = @"qlife";
-NSString *const ACCESS_KEY = @"7827bafd44edad630286e406579ee76d";
-NSString *const SECRET_KEY = @"09d93665b7c63d85607176aeaf6411ee";
-// 生产环境 Boss老板消息服务
-NSString *const MessageBasicURL = @"https://boss-api-dev.aoaosong.com:8009/2.0/";
-NSString *const MessageBasicURLV2 = @"https://boss-api-dev.aoaosong.com:8009/2.0/";
-NSString *const BossUmsPrefix = @"boss";
-NSString *const MessageACCESS_KEY = @"5c0a1988ce6d2a984c1fe925";
-NSString *const MessageSECRET_KEY = @"5c0a1988ce6d2a984c1fe926";
+    // 生产环境 Boss老板
+    NSString *const BossBasicURL = @"https://qlife-api.aoaosong.com/1.0/";
+    NSString *const BossBasicURLV2 = @"https://qlife-api.aoaosong.com/2.0/";
+    NSString *const BossBasicPrefix = @"qlife";
+    NSString *const ACCESS_KEY = @"7827bafd44edad630286e406579ee76d";
+    NSString *const SECRET_KEY = @"09d93665b7c63d85607176aeaf6411ee";
+    // 生产环境 Boss老板消息服务
+    NSString *const MessageBasicURL = @"https://ums-api.aoaosong.com/1.0/";
+    NSString *const MessageBasicURLV2 = @"https://ums-api.aoaosong.com/2.0/";
+    NSString *const BossUmsPrefix = @"boss";
+    NSString *const MessageACCESS_KEY = @"5cf74aa7887d1f0d34985010";
+    NSString *const MessageSECRET_KEY = @"5cf74aa7887d1f0d34985011";
 #else
-// release
-// 生产环境之家 骑士 基础服务
-NSString *const BossBasicURL = @"https://saas-api.aoaosong.com/1.0/";
-NSString *const BossBasicURLV2 = @"https://saas-api.aoaosong.com/2.0/";
-NSString *const BossBasicPrefix = @"boss";
-NSString *const ACCESS_KEY = @"5c779ed4887d1f45fc349753";
-NSString *const SECRET_KEY = @"5c779ed4887d1f45fc349754";
+    // release
+    // 生产环境之家 骑士 基础服务
+    NSString *const BossBasicURL = @"https://saas-api.aoaosong.com/1.0/";
+    NSString *const BossBasicURLV2 = @"https://saas-api.aoaosong.com/2.0/";
+    NSString *const BossBasicPrefix = @"boss";
+    NSString *const ACCESS_KEY = @"5c779ed4887d1f45fc349753";
+    NSString *const SECRET_KEY = @"5c779ed4887d1f45fc349754";
 
-// 生产环境之家 骑士 消息服务
-NSString *const MessageBasicURL = @"https://boss-api-dev.aoaosong.com:8009/2.0/";
-NSString *const MessageBasicURLV2 = @"https://boss-api-dev.aoaosong.com:8009/2.0/";
-NSString *const BossUmsPrefix = @"boss";
-NSString *const MessageACCESS_KEY = @"5c0a1988ce6d2a984c1fe925";
-NSString *const MessageSECRET_KEY = @"5c0a1988ce6d2a984c1fe926";
+    // 生产环境之家 骑士 消息服务
+    NSString *const MessageBasicURL = @"https://ums-api.aoaosong.com/1.0/";
+    NSString *const MessageBasicURLV2 = @"https://ums-api.aoaosong.com/2.0/";
+    NSString *const BossUmsPrefix = @"boss";
+    NSString *const MessageACCESS_KEY = @"5cf74aa7887d1f0d34985032";
+    NSString *const MessageSECRET_KEY = @"5cf74aa7887d1f0d34985033";
 #endif
 
 #endif
--- BossCommonClass/Classes/AllDefine/ConstDataDefine/MQTTDefine.m
+++ BossCommonClass/Classes/AllDefine/ConstDataDefine/MQTTDefine.m
@@ -11,39 +11,60 @@
 
 #ifdef DEBUG
 
-// local
+    // local
 
-//NSString *const mqttServer = @"192.168.10.62";
-//NSInteger const mqttPort   = 1883;
-//
-//NSString *const mqttUserName = @"im_server";
-//NSString *const mqttPassword = @"im_server-123";
-//NSString *const mqttClientId = @"im_server";
-//
-//NSString *const mqttSecretKey = @"cc6803a649ff7f3f036566d1421c4315";
+    //NSString *const mqttServer = @"192.168.10.62";
+    //NSInteger const mqttPort   = 1883;
+    //
+    //NSString *const mqttUserName = @"im_server";
+    //NSString *const mqttPassword = @"im_server-123";
+    //NSString *const mqttClientId = @"im_server";
+    //
+    //NSString *const mqttSecretKey = @"cc6803a649ff7f3f036566d1421c4315";
 
-// dev
+    // dev
 
-NSString *const mqttServer = @"boss-api-dev.aoaosong.com";
-NSInteger const mqttPort   = 1883;
+    NSString *const mqttServer = @"boss-api-dev.aoaosong.com";
+    NSInteger const mqttPort   = 1883;
 
-NSString *const mqttUserName = @"im_server";
-NSString *const mqttPassword = @"im_server-123";
-NSString *const mqttClientId = @"im_server";
+    NSString *const mqttUserName = @"im_server";
+    NSString *const mqttPassword = @"im_server-123";
+    NSString *const mqttClientId = @"im_server";
 
-NSString *const mqttSecretKey = @"cc6803a649ff7f3f036566d1421c4315";
+    NSString *const mqttSecretKey = @"cc6803a649ff7f3f036566d1421c4315";
 
 #else
-// release
 
-NSString *const mqttServer = @"boss-api-dev.aoaosong.com";
-NSInteger const mqttPort   = 1883;
+    // release
 
-NSString *const mqttUserName = @"im_server";
-NSString *const mqttPassword = @"im_server-123";
-NSString *const mqttClientId = @"im_server";
+    NSString *const mqttServer = @"ums-mt.aoaosong.com";
+    NSInteger const mqttPort   = 1883;
 
-NSString *const mqttSecretKey = @"cc6803a649ff7f3f036566d1421c4315";
+#ifdef kBossKnight
+
+    NSString *const mqttUserName = @"aoao_boss_app_ios";
+    NSString *const mqttPassword = @"f241c529e2da4695bb3d6ef4abafd10b";
+
+#elif defined kBossManager
+
+    NSString *const mqttUserName = @"aoao_boss_manager_ios";
+    NSString *const mqttPassword = @"4e3993b160ea47b898bb6b2737b8b6ec";
+
+#elif defined kBossOwner
+
+    NSString *const mqttUserName = @"qlife_ios";
+    NSString *const mqttPassword = @"79a6bcac9a4e43a7aebcb856ae0d9165";
+
+#else
+
+    NSString *const mqttUserName = @"im_server";
+    NSString *const mqttPassword = @"im_server-123";
+
+#endif
+
+NSString *const mqttClientId = @"im_server-";
+
+NSString *const mqttSecretKey = @"6fc6baf2875911e9bc42526af7764f64";
 
 #endif
 
