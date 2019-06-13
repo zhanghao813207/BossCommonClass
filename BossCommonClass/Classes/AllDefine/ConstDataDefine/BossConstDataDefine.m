c910765cfc08f74724237ab3cb408b4c6e88eaae
高炀辉 <insistgyh@foxmail.com>
2019-06-13 14:33:05 +0800

    修改服务端口为8483

--- BossCommonClass/Classes/AllDefine/ConstDataDefine/BossConstDataDefine.m
+++ BossCommonClass/Classes/AllDefine/ConstDataDefine/BossConstDataDefine.m
@@ -17,8 +17,8 @@
 #ifdef kBossOwner
 // dev
 // 测试环境 Boss老板
-NSString *const BossBasicURL = @"https://boss-api-dev.aoaosong.com:8481/1.0/";
-NSString *const BossBasicURLV2 = @"https://boss-api-dev.aoaosong.com:8481/2.0/";
+NSString *const BossBasicURL = @"https://boss-api-dev.aoaosong.com:8483/1.0/";
+NSString *const BossBasicURLV2 = @"https://boss-api-dev.aoaosong.com:8483/2.0/";
 NSString *const BossBasicPrefix = @"qlife";
 NSString *const ACCESS_KEY = @"3f92d896b23d960e529048c2fc2ab762";
 NSString *const SECRET_KEY = @"2fd89206dd6edd7b87b8833ba6bed5a9";
