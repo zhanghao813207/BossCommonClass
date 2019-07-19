bc7f58b3b897fe55dd8b6d13bf2f310c8309d180
高炀辉 <insistgyh@foxmail.com>
2019-07-19 15:22:54 +0800

    添加电子签约

---
+++ BossCommonClass/Classes/Preview/Controllers/PreviewVc.h
@@ -0,0 +1,15 @@
+//
+//  PreviewVc.h
+//  BossManager
+//
+//  Created by 贾远潮 on 2018/8/14.
+//  Copyright © 2018年 贾远潮. All rights reserved.
+//
+
+#import "BossViewController.h"
+
+@interface PreviewVc : BossViewController
+
+@property (nonatomic, strong) NSString *fileURLStr;
+
+@end
