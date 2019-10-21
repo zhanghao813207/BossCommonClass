8f2cea25a539d6e26b67c4bea8997e2f3ec2e33d
qingpingyi <1041700361@qq.com>
2019-10-21 19:31:25 +0800

    时间格式变更

--- BossCommonClass/Classes/Announcement/Date/NSString+Time.m
+++ BossCommonClass/Classes/Announcement/Date/NSString+Time.m
@@ -69,4 +69,16 @@
     NSCalendarUnit calendarUnit = NSCalendarUnitWeekday;    NSDateComponents* theComponents = [calendar components:calendarUnit fromDate:inputDate];    return[weekdays objectAtIndex:theComponents.weekday];
     
 }
+
+/**
+*  变更时间格式满足现实要求, 将20191010变更为2019/10/10格式
+*/
++(NSString *)changeTimeDisplayFormatWithOriginalTime:(NSString *)originalTime{
+    NSDateFormatter *format = [[NSDateFormatter alloc] init];
+    format.dateFormat = @"yyyyMMdd";
+    NSDate *date = [format dateFromString:originalTime];
+    format.dateFormat = @"yyyy/MM/dd";
+    return  [format stringFromDate:date];
+}
+
 @end
