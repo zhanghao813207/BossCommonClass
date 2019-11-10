8e1000009c9da6b7a4bd85f15eae7389928029f1
yjs <>
2019-11-10 18:58:53 +0800

    - 添加获取薪资贷的数据中台基础URL 配置Model类

---
+++ BossCommonClass/Classes/SingletonClass/Account/BossOwner/Model/ConfigSalaryLoan.m
@@ -0,0 +1,78 @@
+//
+//  ConfigSalaryLoan.m
+//  AFNetworking
+//
+//  Created by yjs on 2019/11/10.
+//
+
+#import "ConfigSalaryLoan.h"
+
+
+NSString *const kconfigSalaryLoanUrl = @"url";
+
+@implementation ConfigSalaryLoan
+
+/**
+ * Instantiate the instance using the passed dictionary values to set the properties values
+ */
+
+-(instancetype)initWithDictionary:(NSDictionary *)dictionary
+{
+    self = [super init];
+    if(![dictionary[kconfigSalaryLoanUrl] isKindOfClass:[NSNull class]]){
+        self.url = dictionary[kconfigSalaryLoanUrl];
+    }
+    return self;
+}
+
+
+/**
+ * Returns all the available property values in the form of NSDictionary object where the key is the approperiate json key and the value is the value of the corresponding property
+ */
+-(NSDictionary *)toDictionary
+{
+    NSMutableDictionary * dictionary = [NSMutableDictionary dictionary];
+    if(self.url != nil){
+        dictionary[kconfigSalaryLoanUrl] = self.url;
+    }
+    return dictionary;
+
+}
+
+/**
+ * Implementation of NSCoding encoding method
+ */
+/**
+ * Returns all the available property values in the form of NSDictionary object where the key is the approperiate json key and the value is the value of the corresponding property
+ */
+- (void)encodeWithCoder:(NSCoder *)aCoder
+{
+    if(self.url != nil){
+        [aCoder encodeObject:self.url forKey:kconfigSalaryLoanUrl];
+    }
+
+}
+
+/**
+ * Implementation of NSCoding initWithCoder: method
+ */
+- (instancetype)initWithCoder:(NSCoder *)aDecoder
+{
+    self = [super init];
+    self.url = [aDecoder decodeObjectForKey:kconfigSalaryLoanUrl];
+    return self;
+
+}
+
+/**
+ * Implementation of NSCopying copyWithZone: method
+ */
+- (instancetype)copyWithZone:(NSZone *)zone
+{
+    ConfigSalaryLoan *copy = [ConfigSalaryLoan new];
+
+    copy.url = [self.url copy];
+
+    return copy;
+}
+@end
--- BossCommonClass/Classes/SingletonClass/Account/BossOwner/Model/configModel.h
+++ BossCommonClass/Classes/SingletonClass/Account/BossOwner/Model/configModel.h
@@ -1,14 +1,15 @@
+
 #import <UIKit/UIKit.h>
 #import "configH5.h"
 #import "configMqtt.h"
 #import "configH5.h"
-
+#import "ConfigSalaryLoan.h"
 @interface configModel : NSObject
 
 @property (nonatomic, strong) configH5 * h5;
 @property (nonatomic, strong) configMqtt * mqtt;
 @property (nonatomic, strong) configH5 * ums;
-
+@property (nonatomic, strong) ConfigSalaryLoan * pushu;
 -(instancetype)initWithDictionary:(NSDictionary *)dictionary;
 
 -(NSDictionary *)toDictionary;
