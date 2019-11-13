bf00df522434f5764d9efa7a12902c2dfea27a9a
yjs <>
2019-11-13 14:22:04 +0800

    - 修改文案 - 修改提示框

--- BossCommonClass/Classes/Announcement/VC/SendMessageModule/PhotoManager.m
+++ BossCommonClass/Classes/Announcement/VC/SendMessageModule/PhotoManager.m
@@ -272,11 +272,22 @@ static PhotoManager * sharedSingleton = nil;
                 break;
             }
             case AVAuthorizationStatusDenied: {
-                UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:@"请去-> [设置 - 隐私 - 相机 - SGQRCodeExample] 打开访问开关" preferredStyle:(UIAlertControllerStyleAlert)];
+                NSString *message = @"BOSS当家";
+                   #ifdef kBossKnight
+                       message = @"BOSS骑士";
+                   #endif
+                UIAlertController *alertC = [UIAlertController alertControllerWithTitle:@"温馨提示" message:[NSString stringWithFormat:@"需要您允许%@打开相机权限才能使用哦~去\"设置->隐私->相机\"开启一下吧",message] preferredStyle:(UIAlertControllerStyleAlert)];
                 UIAlertAction *alertA = [UIAlertAction actionWithTitle:@"确定" style:(UIAlertActionStyleDefault) handler:^(UIAlertAction * _Nonnull action) {
                     
                 }];
-                
+                UIAlertAction *cameraAction = [UIAlertAction actionWithTitle:@"设置" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+                    NSURL *url = [NSURL URLWithString:UIApplicationOpenSettingsURLString];
+                    if ([[UIApplication sharedApplication] canOpenURL:url]) {
+                        [[UIApplication sharedApplication] openURL:url];
+                        
+                    }
+                }];
+                [alertC addAction:cameraAction];
                 [alertC addAction:alertA];
                 [[JYCSimpleToolClass getCurrentVC] presentViewController:alertC animated:YES completion:nil];
                 break;
