ae0300e6c49b2d6d147d6574310e99e608c57505
qingpingyi <1041700361@qq.com>
2020-05-26 15:30:46 +0800

    添加保险弹框控制器

---
+++ BossCommonClass/Classes/Safe/SafeTipViewController.h
@@ -0,0 +1,16 @@
+//
+//  SafeTipViewController.h
+//  AFNetworking
+//
+//  Created by 易庆萍 on 2020/5/26.
+//
+
+#import "BossViewController.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface SafeTipViewController : BossViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ BossCommonClass/Classes/Safe/SafeTipViewController.m
@@ -0,0 +1,31 @@
+//
+//  SafeTipViewController.m
+//  AFNetworking
+//
+//  Created by 易庆萍 on 2020/5/26.
+//
+
+#import "SafeTipViewController.h"
+
+@interface SafeTipViewController ()
+
+@end
+
+@implementation SafeTipViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
