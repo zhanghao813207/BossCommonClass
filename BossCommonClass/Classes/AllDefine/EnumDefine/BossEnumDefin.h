96f9c89b88f5c5f53d2632fe05c0eb14a84fba32
董鹏 <739405570@qq.com>
2020-07-01 10:30:19 +0800

    - 添加红冲费用单枚举

--- BossCommonClass/Classes/AllDefine/EnumDefine/BossEnumDefin.h
+++ BossCommonClass/Classes/AllDefine/EnumDefine/BossEnumDefin.h
@@ -315,6 +315,13 @@ typedef NS_ENUM(NSInteger, COST_ORDER_STATE) {
     COST_ORDER_STATE_INIT    =    1, // 待提交
 };
 
+// 费用单类型
+typedef NS_ENUM(NSInteger, ApplicationSubType) {
+    ApplicationSubTypeNormal    =   1,  // 正常费用单
+    ApplicationSubTypeRefund    =  10,  // 退款费用单
+    ApplicationSubTypeRedPush   =  20,  // 红冲费用单
+};
+
 // 成本归属分摊模式
 typedef NS_ENUM(NSUInteger, ALLOCATION_MODE) {
     ALLOCATION_MODE_BALANCE  =  6, // 平均分摊
