3cf17b828de78cf78eb79efb8a72c433d39d67de
高炀辉 <insistgyh@foxmail.com>
2021-10-15 17:35:00 +0800

    添加必要文件

---
+++ BossCommonClass/Classes/CustomRootController/UIView+GetVC.h
@@ -0,0 +1,16 @@
+//
+//  UIView+GetVC.h
+//  BossCommonClass-BossCommonClass
+//
+//  Created by admin on 2019/4/3.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIView (GetVC)
+- (UIViewController *)viewController;
+@end
+
+NS_ASSUME_NONNULL_END
